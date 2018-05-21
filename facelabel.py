2a2d184c92284ddd239c70b9488a6240c423ebd5
piaoliangkb <418508556@qq.com>
2018-05-21 19:22:46 +0800

    add delete page

--- facelabel.py
+++ facelabel.py
@@ -77,9 +77,86 @@ def hello_world():
 
     return render_template("index.html", nameToPicpath=nameToPicpath)
 
+
+@app.route('/adminadminadmin', methods=['GET'])
+def enter_admin_page():
+
+    # list[i] :the name of the sequence
+    try:
+        list = os.listdir(pathOfFaceImg)
+    except OSError:
+        list = []
+    # numOfPeople :number of sequence
+    numOfPeople = len(list)
+    # nameToPicpath :name to filename of this people's picture
+    nameToPicpath = {}
+
+    for i in range(numOfPeople):
+        try:
+            newName = request.args.get(list[i], None)
+        except IndexError:
+            break
+        mergeFlag = False
+        if newName:
+            oldPath = pathOfFaceImg + list[i]
+            newPath = pathOfFaceImg + newName
+            try:
+                os.rename(oldPath, newPath)
+            except FileExistsError:
+                mergeFlag = True
+                # list the files in old folders
+                files = os.listdir(oldPath)
+                for file in files:
+                    fileoldpath = oldPath + "/" + file
+                    filenewpath = newPath + "/" + file
+                    # if exists the file name
+                    if os.path.exists(fileoldpath):
+                        filenewpath = newPath + "/" + str(i) + "-" + file
+                    shutil.copyfile(fileoldpath, filenewpath)
+                shutil.rmtree(oldPath)
+                print("Merge two folders.")
+            if mergeFlag:
+                list.remove(list[i])
+            else:
+                list[i] = newName
+    print(list)
+
+    for i in range(numOfPeople):
+        try:
+            deleteInputValue = list[i] + "-delete"
+            deletename = request.args.get(deleteInputValue, None)
+        except IndexError:
+            break
+        if deletename:
+            filepath = pathOfFaceImg + list[i]
+            shutil.rmtree(filepath)
+            list.remove(list[i])
+
+
+    for i in range(len(list)):
+        pathOfPeople = pathOfFaceImg + list[i]
+        try:
+            filename = os.listdir(pathOfPeople)
+        except OSError:
+            filename = []
+        picpath = []
+        numOfpic = len(filename)
+        if numOfpic > 8:
+            numOfpic = 8
+        for indexOfPic in range(0, numOfpic):
+            filepath = "faceImg/" + list[i] + "/" + filename[indexOfPic]
+            # print the path of picture
+            # print(filepath)
+            picpath.append(filepath)
+        nameToPicpath[list[i]] = picpath
+
+    return render_template("admin.html", nameToPicpath=nameToPicpath)
+
 @app.route('/test')
 def myTest():
     return "hello,world"
+
+
 
 if __name__ == '__main__':
     app.run()
